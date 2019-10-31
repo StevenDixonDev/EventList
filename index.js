597436320716043616829a5e98aeb96410ba1a07
StevenDixonDev <stevedixon1128.dev@gmail.com>
2019-10-31 17:05:11 -0400

    list kinda working

---
+++ index.js
@@ -0,0 +1,62 @@
+// start project
+const QUEUE = "queue";
+const STACK = "stack";
+
+function eventList() {
+  this.list = [];
+  this.style = QUEUE;
+  this.run = () => {
+    // start the list
+    this.next();
+  };
+  this.addEvent = f => {
+    if (this.style === QUEUE) {
+      // add to the back
+      this.list.push(f);
+    } else if (this.style === STACK) {
+      // add to the front
+      this.list = [f, ...this.list];
+    }
+  };
+  this.next = err => {
+    if (err) {
+      return err;
+    }
+    const [current] = this.list.slice(0, 1);
+    this.list = this.list.slice(1);
+    if (current && typeof current === "function") {
+      return current(this.next);
+    } else {
+      console.log("done");
+    }
+  };
+}
+
+const El = new eventList();
+
+// define a function that takes the next item
+function myAwesomeFunction(next) {
+  console.log("hi");
+  next();
+}
+
+const test = next => {
+  console.log("hmmm");
+  next();
+};
+
+function timeOutTest(next) {
+  setTimeout(() => {
+    console.log("I am timed!");
+    next();
+  });
+}
+
+El.style = QUEUE;
+
+El.addEvent(myAwesomeFunction);
+El.addEvent(timeOutTest);
+
+El.run();
+
+El.addEvent(test);
---
+++ readme.md
@@ -0,0 +1,18 @@
+# Eventlist
+
+Eventlist is a ? for and event system for games or other event based apps.
+
+This is my attempt to code a management system for these items.
+
+## Core Idea
+
+Start with a Linked list style object
+
+give the user the ability to add items to the list 
+
+let complexity evolve into the system in the form of features.
+
+## Ideas
+
+Implement socket io into the mix? or make it easy for a user to do so
+
